2efb43911012f5d1d359462c346c9df399dcdbc7
Ansh-singh-rajput <ansh9754@gamil.com>
2024-05-17 23:17:42 +0530

    api in react

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const app = express()
 const cookieParser = require('cookie-parser')
-app.use(cookieParser())
+app.use(cookieParser()) // for getting token in auth
 
 const cors = require('cors')
+app.use(cors()) // for api communication in react
 
 const dotenv= require('dotenv')
 dotenv.config({path:'./.env'})
 const web = require('./routes/web')
 const connectdb = require('./db/connectdb')
 //file upload
-const fileUpload=require('express-fileupload')
+const fileUpload=require('express-fileupload') //for file upload
 //tempfiles uploaderz
 app.use(fileUpload({useTempFiles:true}))
 
@@ -36,9 +37,7 @@ app.use('/api',web)
 
 
 
-
 //server create
 app.listen(process.env.PORT,()=>{
     console.log(`server running on localhost: ${process.env.PORT}`)
 })
-
