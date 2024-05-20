530e2a94e07329eb2af90523a51355e610a68cbb
Ansh-singh-rajput <ansh9754@gamil.com>
2024-05-20 18:31:27 +0530

    categorycontroller change

--- models/category.js
+++ models/category.js
@@ -5,7 +5,8 @@ const categorySchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  image: { // Change 'images' to 'image'
+  // Change 'images' to 'image'
+  image: { 
     public_id: {
       type: String,
       required: true,
